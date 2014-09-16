774fadf76f7b46be218fda874cf9e8dc9df8688e
troyth <troyth@gmail.com>
2014-09-16 09:53:09 -0400

    2 items in carousel

--- app/public/javascript/main.js
+++ app/public/javascript/main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
   $('#tmpltzr').width( window.innerWidth - 400 );
 
-  $(".owl-carousel").owlCarousel();
+  $(".owl-carousel").owlCarousel({
+    items : 2
+  });
 
 });
