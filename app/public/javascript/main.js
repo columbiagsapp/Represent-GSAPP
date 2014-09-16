7bd2141a39d19a3fa19c3e5416f012eba7147400
troyth <troyth@gmail.com>
2014-09-16 09:55:32 -0400

    test options

--- app/public/javascript/main.js
+++ app/public/javascript/main.js
@@ -3,7 +3,14 @@ $(document).ready(function(){
   $('#tmpltzr').width( window.innerWidth - 400 );
 
   $(".owl-carousel").owlCarousel({
-    items : 2
+    items : 1.5,
+    slideSpeed : 1000,
+    autoPlay : true,
+    stopOnHover : true,
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window.innerWidth - 400,
+
   });
 
 });
