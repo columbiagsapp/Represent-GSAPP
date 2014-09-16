fe94fd5be0adb622359367a4b0475b2d8078b2bc
troyth <troyth@gmail.com>
2014-09-16 10:00:13 -0400

    repgsapp-wrapper responsive base

--- app/public/javascript/main.js
+++ app/public/javascript/main.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     stopOnHover : true,
     responsive: true,
     responsiveRefreshRate : 200,
-    responsiveBaseWidth: window,
+    responsiveBaseWidth: '#repgsapp-wrapper',
     itemsScaleUp: true
 
   });
