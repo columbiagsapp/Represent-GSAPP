97f05ae97af5b1d5d0464ef467841881f014046c
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-09-18 09:58:32 -0400

    stats page initial commit
    updated .gitignore with node_modules, .DS_Store
    set up Grunt for stats pg

---
+++ test/Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            build: {
+                files: {
+                    'js/script.min.js': ['js/script.js']
+                }
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    compass: true,
+                    style: 'compressed'
+                },
+                files: {
+                    'css/style.css': 'sass/style.scss'
+                }
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['> 1%']
+            },
+            no_dest: {
+                src: 'css/style.css'
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['uglify'],
+                options: {
+                    spawn: false,
+                }
+            },
+            css: {
+                files: ['sass/*.scss', 'css/style.css'],
+                tasks: ['sass', 'autoprefixer'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['uglify', 'sass', 'autoprefixer', 'watch']);
+
+};
