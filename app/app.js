d134e9b0753a900aaa754839d954533341186536
troyth <troyth@gmail.com>
2014-09-18 10:12:14 -0400

    added sass support

--- app/app.js
+++ app/app.js
@@ -7,7 +7,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , sass = require('node-sass');
 
 var app = express();
 
@@ -21,6 +22,15 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+// add SASS support for gradient bg
+app.use(
+   sass.middleware({
+     src: __dirname + '/sass', //where the sass files are
+     dest: __dirname + '/public', //where css should go
+     debug: true // obvious
+   })
+);
 
 // development only
 if ('development' == app.get('env')) {
