479bbd65e8773aff5a52f081d927998e4f8412c5
troyth <troyth@gmail.com>
2014-09-18 22:59:46 -0400

    don't redirect on status change just send 200

--- app/controllers/images.js
+++ app/controllers/images.js
@@ -264,7 +264,7 @@ exports.setStatus = function(req, res, id, status){
           console.log('setStatus()::error attempting to save image status');
           res.send(500, 'image status not saved, server error on save attempt');
         } else {
-          res.redirect('/edit/' + status);
+          res.send(200);
         }
       });// end save
 
