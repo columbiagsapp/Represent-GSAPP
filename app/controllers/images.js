cee6dad5b30c5051668671ec370ba4649d362a24
troyth <troyth@gmail.com>
2014-09-18 23:03:12 -0400

    remove item on click of edit-controls

--- app/controllers/images.js
+++ app/controllers/images.js
@@ -264,7 +264,8 @@ exports.setStatus = function(req, res, id, status){
           console.log('setStatus()::error attempting to save image status');
           res.send(500, 'image status not saved, server error on save attempt');
         } else {
-          res.send(200);
+          // do nothing, remain on the page
+          //res.send(200);
         }
       });// end save
 
