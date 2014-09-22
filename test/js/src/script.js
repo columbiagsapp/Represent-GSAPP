70628b67c5ec27ca98b848471a6a2975d32632de
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-09-22 10:24:04 -0400

    fit all chart bars on screen

--- test/js/src/script.js
+++ test/js/src/script.js
@@ -2,7 +2,7 @@
 
 	var chart = new Snap('#stats'),
 		apiURL = 'stats.json', // change to URL string when API is ready
-		columnWidth = 110, // might want to dynamically set this from text width
+		columnWidth = 50, // might want to dynamically set this from text width
 		data;
 
 	var height = +chart.attr('height'),
@@ -20,20 +20,21 @@
 		for ( var i = 0; i < data.programs.length; i++ ) {
 
 			// create text
-			var text = chart.text( columnWidth * i, height - 5, data.programs[i].name).attr({
+			var text = chart.text( columnWidth * i + columnWidth / 2, height - 100, data.programs[i].name).attr({
 				'class': 'chart-label ' + colors[i % colors.length]
 			});
+			text.transform(Snap.matrix().rotate(-90, columnWidth * i + columnWidth / 2 + 5, height - 100).toTransformString());
 
 			var textWidth = $(text.node).width();
 
 			text.attr({
-				x: ( columnWidth * i ) + ( columnWidth - textWidth ) / 2
+				x: ( columnWidth * i ) + ( columnWidth - textWidth )
 			});
 
 			// create bar
 			// TODO: height should be relative to program with highest # of total images,
 			// which is 100% height of the chart
-			var bar = chart.rect( columnWidth * i + 10, height - data.programs[i].total_images - 25, columnWidth - 20, data.programs[i].total_images ).attr({
+			var bar = chart.rect( columnWidth * i + columnWidth / 10, height - data.programs[i].total_images - 145, columnWidth - columnWidth / 5, data.programs[i].total_images ).attr({
 				'class': colors[i % colors.length]
 			});
 		}
@@ -52,8 +53,30 @@
 		});
 	}
 
+	var isMousedown = false,
+		originator;
+
+	function mouseIsDown(e) {
+		isMousedown = true;
+		originator = e.offsetX;
+	}
+
+	function mouseIsUp() {
+		isMousedown = false;
+	}
+
+	function moveChart(e) {
+		
+		if ( isMousedown ) {
+			$(this).css({
+				left: (e.pageX - originator)
+			});
+		}
+	}
+
 	function init() {
 		retrieveData();
+		// $('#stats').mousedown(mouseIsDown).mouseup(mouseIsUp).mousemove(moveChart);
 	}
 
 	init();
