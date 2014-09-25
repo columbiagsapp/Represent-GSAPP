86f350e58a9fac48b21e4fb3b0d18212f3f8008a
Troy Conrad Therrien <troyth@gmail.com>
2014-09-25 09:48:46 -0400

    log

--- app/public/javascript/main.js
+++ app/public/javascript/main.js
@@ -14,6 +14,42 @@ $(document).ready(function(){
 
   });
 
-  
+
+
+
+  // Gradient background
+
+  // make sure this matches all the colors in scss/_vars.scss
+  var colors = ['red', 'red-orange', 'orange', 'pink', 'salmon', 'blue', 'blue-alt', 'purple', 'violet', 'fuchsia', 'red-purple'];
+
+  var gradient = $('.gradient');
+
+  function gradientCycle() {
+    console.log('gradientCycle()');
+    var classes = gradient.attr('class').split(' '),
+      oldClass;
+    for ( var i = 0; i < classes.length; i++ ) {
+      if ( classes[i] !== 'gradient') {
+        oldClass = classes[i];
+        oldClass = oldClass.replace('gradient-', '');
+        break;
+      }
+    }
+    var newClass = colors[(colors.indexOf(oldClass) + 1) % colors.length];
+    var newGradient = $('<div class="gradient gradient-' + newClass + ' faded">');
+    $('.gradient-' + oldClass).last().after(newGradient);
+
+    //gradient.addClass('faded');
+    setTimeout(function(){
+      newGradient.removeClass('faded');
+    }, 100);
+
+    setTimeout(function(){
+      gradient = newGradient;
+      gradientCycle();
+    }, 5000);
+  }
+  gradientCycle();
+
 
 });
