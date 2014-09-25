6d74f56efb249fe4e718cd2278a0523e6ed3996f
Troy Conrad Therrien <troyth@gmail.com>
2014-09-25 10:14:25 -0400

    remove autoplay

--- app/public/javascript/main.js
+++ app/public/javascript/main.js
@@ -2,18 +2,6 @@ $(document).ready(function(){
 
   $('#tmpltzr').width( window.innerWidth - 400 );
 
-  $(".owl-carousel").owlCarousel({
-    items : 3,
-    slideSpeed : 1000,
-    autoPlay : true,
-    stopOnHover : true,
-    responsive: true,
-    responsiveRefreshRate : 200,
-    responsiveBaseWidth: '#repgsapp-wrapper',
-    itemsScaleUp: true
-
-  });
-
 
 
 
