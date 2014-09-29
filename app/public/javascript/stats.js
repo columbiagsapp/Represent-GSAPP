3e61936ad904aea07a7ae08777326f20b5016ef0
Troy Conrad Therrien <troyth@gmail.com>
2014-09-29 09:05:44 -0400

    moved stats.js to own file

--- app/public/javascript/stats.js
+++ app/public/javascript/stats.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
   var chart = new Snap('#stats'),
     apiURL = 'stats.json', // change to URL string when API is ready
     columnWidth = 50, // might want to dynamically set this from text width
@@ -76,39 +77,11 @@ $(document).ready(function(){
     }
   }
 
-  function gradientCycle() {
-    var classes = gradient.attr('class').split(' '),
-      oldClass;
-    for ( var i = 0; i < classes.length; i++ ) {
-      if ( classes[i] !== 'gradient') {
-        oldClass = classes[i];
-        oldClass = oldClass.replace('gradient-', '');
-        break;
-      }
-    }
-    var newClass = colors[(colors.indexOf(oldClass) + 1) % colors.length];
-    var newGradient = $('<div class="gradient gradient-' + newClass + ' faded">');
-    $('.gradient-' + oldClass).last().after(newGradient);
-
-    //gradient.addClass('faded');
-    setTimeout(function(){
-      newGradient.removeClass('faded');
-    }, 100);
-
-    setTimeout(function(){
-      gradient.remove();
-      gradient = newGradient;
-      gradientCycle();
-    }, 5000);
-  }
-  gradientCycle();
-
   function init() {
     retrieveData();
     // $('#stats').mousedown(mouseIsDown).mouseup(mouseIsUp).mousemove(moveChart);
   }
 
   init();
-
 
 });
